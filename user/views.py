2cdbae3a0ad63f749b18c2cb4d80537d0b435b70
Bismark Kadogbe <neroaboagye@gmail.com>
2022-12-26 22:09:27 +0000

    wrote an update function on profile

--- user/views.py
+++ user/views.py
@@ -20,9 +20,18 @@ def register(request):
 
 
 def profile(request):
+
+    return render(request,'user/profile.html')
+
+
+def profile_update(request):
     if request.method =="POST":
-        user_form = UserUpdateForm(request.POST, instance=request.user)
+        user_form = UserUpdateForm(request.POST, instance=request.user) #The instance allows the form to be prefield by the data available
         profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('user-profile')
     else:
         user_form = UserUpdateForm( instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
@@ -32,11 +41,7 @@ def profile(request):
         'profile_form': profile_form,
     }
 
-    return render(request,'user/profile.html')
 
-
-def profile_update(request):
-
-    return render(request,'user/profile_update.html')
+    return render(request,'user/profile_update.html',context)
 
 
